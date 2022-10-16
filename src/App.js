79d0510fe435d896da206c68fb8c5368561fbf31
Andy <alipinski@mail.com>
2022-10-16 18:06:54 +0200

    controlled input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 
-import Counter from './components/Counter';
-import UseStateArray from './components/UseStateArray';
-import UseStateObjects from './components/UseStateObjects';
-import Conditionals3 from './components/Conditionals3';
-import ConditionalsWithProps from './components/ConditionalsWithProps/ConditionalsWIthProps';
-import UseEffectCleanup from './components/UseEffectCleanup';
+// import Counter from './components/Counter';
+// import UseStateArray from './components/UseStateArray';
+// import UseStateObjects from './components/UseStateObjects';
+// import Conditionals3 from './components/Conditionals3';
+// import ConditionalsWithProps from './components/ConditionalsWithProps/ConditionalsWIthProps';
+// import UseEffectCleanup from './components/UseEffectCleanup';
+import ControlledInputs from './components/forms/ControlledInputs';
 
 function App() {
   return (
     <div>
-      <UseEffectCleanup />
+      <ControlledInputs />
     </div>
   );
 }
